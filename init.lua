4d11699edc401495ee6ed7a32adc91170f33236e
Goody-debug <goody-debug@proton.me>
2026-05-14 17:58:21 +0530

    add debugging

--- init.lua
+++ init.lua
@@ -325,10 +325,55 @@ require("which-key").setup({
 -- Debugging & Godot
 pack_add({
     "https://github.com/mfussenegger/nvim-dap",
+    "https://github.com/jay-babu/mason-nvim-dap.nvim",
     "https://github.com/rcarriga/nvim-dap-ui",
+    "https://github.com/nvim-neotest/nvim-nio",
     "https://github.com/Mathijs-Bakker/godotdev.nvim"
 })
 require("godotdev").setup()
+require("mason-nvim-dap").setup({
+    ensure_installed = {
+        "Codelldb",
+        "python",
+    }
+})
+
+local dap = require('dap')
+local dapui = require('dapui')
+
+dapui.setup()
+dap.listeners.after.event_initialized["dapui_config"] = function()
+    dapui.open()
+end
+dap.listeners.before.event_terminated["dapui_config"] = function()
+    dapui.close()
+end
+dap.listeners.before.event_exited["dapui_config"] = function()
+    dapui.close()
+end
+
+-- keymaps
+vim.keymap.set('n', '<leader>du', function() dapui.toggle() end, { desc = 'DAP: Toggle UI' })
+vim.keymap.set('n', '<leader>dh', function() dapui.eval() end, { desc = 'DAP UI: Evaluate (Hover)' })
+vim.keymap.set('v', '<leader>dh', function() dapui.eval() end, { desc = 'DAP UI: Evaluate Selection' })
+vim.keymap.set('n', '<leader>df', function() dapui.float_element() end, { desc = 'DAP UI: Float Element' })
+
+vim.keymap.set('n', '<F5>', function() dap.continue() end, { desc = 'DAP: Continue/Start' })
+vim.keymap.set('n', '<F10>', function() dap.step_over() end, { desc = 'DAP: Step Over' })
+vim.keymap.set('n', '<F11>', function() dap.step_into() end, { desc = 'DAP: Step Into' })
+vim.keymap.set('n', '<F12>', function() dap.step_out() end, { desc = 'DAP: Step Out' })
+
+vim.keymap.set('n', '<leader>db', function() dap.toggle_breakpoint() end, { desc = 'DAP: Toggle Breakpoint' })
+vim.keymap.set('n', '<leader>dB', function()
+    dap.set_breakpoint(vim.fn.input('Breakpoint condition: '))
+end, { desc = 'DAP: Conditional Breakpoint' })
+vim.keymap.set('n', '<leader>dp', function()
+    dap.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))
+end, { desc = 'DAP: Log Point' })
+vim.keymap.set('n', '<leader>dc', function() dap.clear_breakpoints() end, { desc = 'DAP: Clear Breakpoints' })
+
+vim.keymap.set('n', '<leader>dl', function() dap.run_last() end, { desc = 'DAP: Run Last Session' })
+vim.keymap.set('n', '<leader>dt', function() dap.terminate() end, { desc = 'DAP: Terminate Session' })
 
 -- Garbage Day
 pack_add({ "https://github.com/zeioth/garbage-day.nvim" })
